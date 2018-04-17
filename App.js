ec81c978bb414d67544e9c1de8977fb1df21a380
Iban Balasch <iban.juanico@gmail.com>
2018-04-17 16:51:56 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CardSwiper from './screens/CardSwiper';
 import Loading from './screens/Loading';
+import Home from './screens/Home';
+import CardSwiper from './screens/CardSwiper';
 import { StackNavigator } from 'react-navigation';
 
 const AppStackNavigator = StackNavigator({
-  LoadingScreen: { screen: Loading }, 
-  CardSwiperScreen: { screen: CardSwiper, 
-                      navigationOptions: {
-                        header: false,
-                    }}
+  LoadingScreen: { screen: Loading },   
+  HomeScreen: { screen: Home, navigationOptions: { header: false, }},
+  CardSwiperScreen: { screen: CardSwiper, navigationOptions: { header: false, }}
 });
 
 console.disableYellowBox = true;
